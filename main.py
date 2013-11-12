e8525f4d7d3be63b4b4afaa1fc120df9ac20b670
metaperl <metaperl@gmail.com>
2013-11-12 09:22:49 -0500

    fixes

--- main.py
+++ main.py
@@ -1,4 +1,19 @@
-from src import service
+# core
 
-o = service.CEX()
-print o.ticker()
+# 3rd party
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+# local
+import login
+
+base_url = 'https://cex.io'
+
+driver = webdriver.Firefox()
+driver.get(base_url)
+
+driver.find_element_by_class_name('dropdown-toggle').click()
+driver.find_element_by_name('username').send_keys(login.username)
+driver.find_element_by_name('password').send_keys(login.password)
+
+elem.click()
